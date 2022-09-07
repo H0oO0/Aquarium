388e14a6f81286df38ebc5184499780620c2e073
H0oO0 <96241466+H0oO0@users.noreply.github.com>
2022-09-07 20:04:47 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -166,10 +166,10 @@
 
         $(".animal_right").click(function () {
 
-            var src2 = "/images/animals/up" + showAnimal + ".png";
+            var src2 = "../images/animals/up" + showAnimal + ".png";
 
             $(".up").css({
-                "background": "url(/images/animals/back" + showAnimal + ".jpg)",
+                "background": "url(../images/animals/back" + showAnimal + ".jpg)",
                 "background-size": 100 + "%"
             });
             $(".animal>img").attr("src", src2);
@@ -181,10 +181,10 @@
         })
         $(".animal_left").click(function () {
 
-            var src2 = "/images/animals/up" + showAnimal + ".png";
+            var src2 = "../images/animals/up" + showAnimal + ".png";
 
             $(".up").css({
-                "background": "url(/images/animals/back" + showAnimal + ".jpg)",
+                "background": "url(../images/animals/back" + showAnimal + ".jpg)",
                 "background-size": 100 + "%"
             });
             $(".animal>img").attr("src", src2);
